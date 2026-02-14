f4019d15108e8a0670a6bb73dbe0cb68e13b0ccd
satyabhat09 <satyabhat7@gmail.com>
2026-02-14 23:41:52 +0530

    multiplication

--- add.py
+++ add.py
@@ -2,4 +2,6 @@ num1 = int(input("enter first number :"))
 num2 = int(input("enter second number"))
 sum = num1+num2
 print("addition is:",sum)
+mul =num1*num2
+print("multiplication is",mul)
            
